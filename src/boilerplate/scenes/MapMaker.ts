1ed351c41c6983d35bd6ad8387393c53c6daabb4
Nick B <nicholas.balestrieri@gmail.com>
2019-06-23 16:52:22 -0400

    level creation logic for castle and desert makes sense

--- src/boilerplate/scenes/MapMaker.ts
+++ src/boilerplate/scenes/MapMaker.ts
@@ -8,18 +8,55 @@ const MapMaker= (type:string):Array<number> =>{
 
   let level:number = 125;
 
+  let castle_towerflag:boolean = false
+  let castle_towerEnd:number
+
   for (let i=0; i < 300; i++){
 
     let levelOffset:number
-    if(level===149){
-      levelOffset = 1
+
+
+    if(level===140){
+
+      levelOffset = -1
+
+    }else if (i>280&&type!=="Castle"){
+      if(level>125){
+        levelOffset=-1
+      }else if(level<125){
+        levelOffset=1
+      }
+      else{levelOffset = 0}
+
     } else if(type==="Castle"){
+
       levelOffset = 0
+
+      if(i===5||i===290){
+        levelOffset = -40
+      } else if(i===10||i===295){
+        levelOffset = 40
+      } else {
+
+        if(!castle_towerflag && i > 20 && i < 280){
+        var towerChance = Math.random()
+          if(towerChance<0.5 && level<50){
+            levelOffset=-5
+            castle_towerEnd = i+Math.ceil(Math.random()*10)+2
+            castle_towerflag = true
+          }
+        }
+        if(i===castle_towerEnd){
+          levelOffset=5
+          castle_towerflag = false
+        }
+      }
     } else if(type==="Desert") {
       levelOffset = Math.floor((Math.random()*3)-1);
     } else if (type==="Mountain") {
       levelOffset = Math.floor((Math.random()*5)-2);
-
+    } else {
+      levelOffset = Math.floor((Math.random()*3)-1);
     }
 
     const column:number[] = [];
